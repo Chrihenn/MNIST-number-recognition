018e5f341f83a1c5f895fc7041045522ca93465f
bendikla <bendikla@live.no>
2018-11-25 16:02:38 +0100

    Update svm_minst_load_digits.py

--- svm_minst_load_digits.py
+++ svm_minst_load_digits.py
@@ -4,6 +4,9 @@ from sklearn import datasets
 from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix
+from skimage import exposure
+import imutils
+import cv2
 
 # KILDER:
 # https://martin-thoma.com/svm-with-sklearn/
@@ -37,3 +40,15 @@ print("Confusion matrix:\n", confusion_matrix(testLabels.ravel(), predicted))
 
 print('\n', classification_report(testLabels.ravel(), predicted))
 print("\nAccuracy: ",  clf.score(testData, testLabels))
+
+for i in np.random.randint(0, high=len(testLabels), size=(5,)):
+    image = testData[i]
+    prediction = clf.predict(image.reshape(1, -1))[0]
+
+    image = image.reshape((8, 8)).astype("uint8")
+    image = exposure.rescale_intensity(image, out_range=(0, 255))
+    image = imutils.resize(image, width=32, inter=cv2.INTER_CUBIC)
+
+    print("I think that digit is: {}".format(prediction))
+    cv2.imshow("Image", image)
+    cv2.waitKey(0)
